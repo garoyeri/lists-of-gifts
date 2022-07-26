1c845e1786cf52fccee7b6b183c13ae9f6b06b8e
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2022-07-26 18:15:21 -0500

    Clean up look of list items

--- app/components/list-item-view.tsx
+++ app/components/list-item-view.tsx
@@ -1,18 +1,26 @@
 import type { GiftListItem } from "@prisma/client"
+import { Link } from "@remix-run/react";
 
 export default function GiftListItemView({
     item,
     isOwner,
-} : {
+}: {
     item: Pick<GiftListItem, "title" | "details" | "id" | "url" | "imageUrl">;
     isOwner: boolean;
 }) {
     return (
-        <div className="bg-secondary w-full h-60">
-            <h4 className="text-lg">{item.title}</h4>
-            <article className="prose">
-                {item.details}
-            </article>
+        <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src={item.imageUrl} alt={item.title} /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{item.title}</h2>
+                <p>{item.details}</p>
+                <div className="card-actions justify-end">
+                    {isOwner ? (
+                        <Link to={`edit/${item.id}`} className="btn btn-secondary">Edit</Link>
+                    ) : null}
+                    <a href={item.url} className="btn btn-primary">Buy Now</a>
+                </div>
+            </div>
         </div>
     )
 }
--- app/routes/lists/$listId.tsx
+++ app/routes/lists/$listId.tsx
@@ -30,12 +30,12 @@ export default function ListDetailsPage() {
             <h3 className="text-2xl font-bold block w-full mb-4">{data.list.title}</h3>
             <div className="container">
                 <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "" }} isOwner={true} />
-                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "" }} isOwner={true} />
-                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "" }} isOwner={true} />
-                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "" }} isOwner={true} />
-                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "" }} isOwner={true} />
-                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "" }} isOwner={true} />
+                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "https://www.google.com", imageUrl: "https://placeimg.com/400/225/arch" }} isOwner={true} />
+                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "https://placeimg.com/400/225/arch" }} isOwner={true} />
+                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "https://placeimg.com/400/225/arch" }} isOwner={true} />
+                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "https://placeimg.com/400/225/arch" }} isOwner={true} />
+                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "https://placeimg.com/400/225/arch" }} isOwner={true} />
+                    <GiftListItemView item={{ title: "Item 1", id: "123", details: "abc", url: "", imageUrl: "https://placeimg.com/400/225/arch" }} isOwner={false} />
                 </div>
             </div>
         </div>
