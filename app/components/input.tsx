2ef02fd51a13b22a046a846f1c9902e4dfe8e2a0
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2022-07-27 18:16:19 -0500

    Add shared input component to reduce copypasta

---
+++ app/components/input.tsx
@@ -0,0 +1,33 @@
+export default function Input({
+    field, error, label, id, size = "lg", required = false
+} : {
+    field?: string;
+    error?: string;
+    label: string;
+    id: string;
+    size?: string;
+    required?: boolean;
+}) {
+    return (
+        <div className="form-control">
+        <label className="label" htmlFor={id}>
+            <span className={`label-text text-${size}`}>{label}</span>
+        </label>
+        <input
+            name={id} id={id}
+            defaultValue={field}
+            className={`input input-bordered input-${size}`}
+            aria-invalid={error ? true : undefined}
+            aria-errormessage={
+                error ? `${id}-error` : undefined
+            }
+            required={required ?? undefined}
+        />
+        {error && (
+            <div className="pt-1 text-error" id={`${id}-error`}>
+                {error}
+            </div>
+        )}
+    </div>
+    );
+}
